4af1a335a08503c79dac449d1c1001182f9916f2
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2020-07-22 07:28:00 +0900

    fix examples/events.rs

--- examples/events.rs
+++ examples/events.rs
@@ -1,63 +1,78 @@
-use wita::{Context, EventHandler, KeyCode, KeyState, MouseButton, MouseState, RunType, Window, WindowBuilder};
-
 struct Application;
 
-impl EventHandler for Application {
-    fn activated(&mut self, _: &Window) {
+impl wita::EventHandler for Application {
+    fn activated(&mut self, _: &wita::Window) {
         println!("activated");
     }
 
-    fn inactivated(&mut self, _: &Window) {
+    fn inactivated(&mut self, _: &wita::Window) {
         println!("inactivated");
     }
 
-    fn closed(&mut self, _: &Window) {
+    fn closed(&mut self, _: &wita::Window) {
         println!("closed");
     }
 
-    fn resized(&mut self, _: &Window, size: wita::PhysicalSize<f32>) {
+    fn resized(&mut self, _: &wita::Window, size: wita::PhysicalSize<f32>) {
         println!("resized: {:?}", size);
     }
 
-    fn moved(&mut self, _: &Window, pt: wita::ScreenPosition) {
+    fn moved(&mut self, _: &wita::Window, pt: wita::ScreenPosition) {
         println!("moved: {:?}", pt);
     }
 
-    fn dpi_changed(&mut self, window: &Window) {
+    fn dpi_changed(&mut self, window: &wita::Window) {
         println!("dpi changed: {}", window.scale_factor());
     }
 
-    fn mouse_input(&mut self, _: &Window, button: MouseButton, button_state: KeyState, mouse_state: MouseState) {
+    fn mouse_input(&mut self, _: &wita::Window, button: wita::MouseButton, button_state: wita::KeyState, mouse_state: wita::MouseState) {
         println!("mouse_input: {:?}, {:?}, {:?}", button, button_state, mouse_state);
     }
 
-    fn cursor_moved(&mut self, _: &Window, state: MouseState) {
+    fn cursor_moved(&mut self, _: &wita::Window, state: wita::MouseState) {
         println!("cursor moved: {:?}", state);
     }
 
-    fn cursor_entered(&mut self, _: &Window, state: MouseState) {
+    fn cursor_entered(&mut self, _: &wita::Window, state: wita::MouseState) {
         println!("cursor entered: {:?}", state);
     }
 
-    fn cursor_leaved(&mut self, _: &Window, state: MouseState) {
+    fn cursor_leaved(&mut self, _: &wita::Window, state: wita::MouseState) {
         println!("cursor leaved: {:?}", state);
     }
 
-    fn key_input(&mut self, _: &Window, code: KeyCode, state: KeyState) {
+    fn key_input(&mut self, _: &wita::Window, code: wita::KeyCode, state: wita::KeyState) {
         println!("key input: {:?}, {:?}", code, state);
     }
 
-    fn char_input(&mut self, _: &Window, c: char) {
+    fn char_input(&mut self, _: &wita::Window, c: char) {
         if c.is_control() || c.is_whitespace() {
             println!("char input: 0x{:02x}", c as u16);
         } else {
             println!("char input: {}", c);
         }
     }
+
+    fn ime_start_composition(&mut self, _: &wita::Window) {
+        println!("ime start composition");
+    }
+
+    fn ime_composition(
+        &mut self,
+        _: &wita::Window,
+        comp: &wita::ime::Composition,
+        candidate: Option<&wita::ime::CandidateList>,
+    ) {
+        println!("ime composition: {:?} {:?}", comp, candidate);
+    }
+
+    fn ime_end_composition(&mut self, _: &wita::Window, s: Option<&str>) {
+        println!("ime end composition: {:?}", s);
+    }
 }
 
 fn main() {
-    let context = Context::new();
-    let _window = WindowBuilder::new().title("wita events").build(&context);
-    context.run(RunType::Wait, Application);
+    let context = wita::Context::new();
+    let _window = wita::WindowBuilder::new().title("wita events").build(&context);
+    context.run(wita::RunType::Wait, Application);
 }
