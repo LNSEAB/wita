fabf9960a093cfde77b7ca23a393031b4c176c21
LNSEAB <691752+LNSEAB@users.noreply.github.com>
2020-07-24 10:27:58 +0900

    update examples/drop_files.rs

--- examples/drop_files.rs
+++ examples/drop_files.rs
@@ -1,10 +1,34 @@
 use std::path::Path;
 
-struct Application;
+struct Application {
+    accept_drag_files: bool,
+}
+
+impl Application {
+    fn new() -> Self {
+        Self {
+            accept_drag_files: true,
+        }
+    }
+}
 
 impl wita::EventHandler for Application {
     fn drop_files(&mut self, _: &wita::Window, paths: &[&Path], position: wita::LogicalPosition<f32>) {
         println!("drop files: {:?}, {:?}", paths, position);
+    }
+
+    fn key_input(&mut self, window: &wita::Window, code: wita::KeyCode, state: wita::KeyState) {
+        if state == wita::KeyState::Pressed {
+            if code.vkey == wita::VirtualKey::Char('T') {
+                self.accept_drag_files = !self.accept_drag_files;
+                window.accept_drag_files(self.accept_drag_files);
+                if self.accept_drag_files {
+                    println!("enabled accept_drag_files");
+                } else {
+                    println!("disabled accept_drag_files");
+                }
+            }
+        }
     }
 }
 
@@ -14,5 +38,5 @@ fn main() {
         .title("wita drop files")
         .accept_drag_files(true)
         .build(&context);
-    context.run(wita::RunType::Wait, Application);
+    context.run(wita::RunType::Wait, Application::new());
 }
